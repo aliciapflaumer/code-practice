79fdb574b649de2761ab6b1b51e29b4ebb0a82c7
aliciapflaumer <alpfla7@gmail.com>
2018-05-13 15:39:16 -0400

    Added a comment about Math.ceil()

--- index.js
+++ index.js
@@ -29,5 +29,6 @@ console.log(firstDuplicate(a))
 // Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
 
 function CenturyFromYear (year) {
+  // Use `Math.ceil()` to round up
   return Math.ceil(year/100)
 }
