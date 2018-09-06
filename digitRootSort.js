dd2e0304c8aaa255c8a866a4a11430353382a5ed
aliciapflaumer <alpfla7@gmail.com>
2018-09-06 18:51:46 -0400

    Created new file for digitRootSort
    - Practice solving digitRoot sort algorithm

---
+++ digitRootSort.js
@@ -0,0 +1,15 @@
+a = [13, 20, 7, 4]
+// digital root 4, 2, 7, 4
+// output [20, 4, 13, 7]
+
+function digitRootSort(a) {
+  for (let i = 0; i < a.length; i++) {
+    // if a comes before b
+        // then digitRoot_a is <= digitRoot_b
+
+    // if else two numbers have same digitRoot
+        // the smaller number should come first
+
+  }
+}
+console.log(digitRootSort(a))
